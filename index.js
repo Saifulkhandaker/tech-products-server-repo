7ed1b7fbff75d5977c9a5bbe6de7338a1aa16748
Saifulkhandaker <khsaiful67@gmail.com>
2023-11-26 19:42:53 +0600

    jwt token added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -35,8 +36,34 @@ async function run() {
     const reviewCollection = client.db("techDb").collection("reviews");
 
 
+
+    // jwt related api
+    app.post('/jwt', async(req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+      res.send({token})
+    })
+
+
+     // middleware------------
+    const verifytoken = (req, res, next) => {
+      // console.log('insider verify token', req.headers.authorization);
+      if(!req.headers.authorization){
+        return res.status(401).send({message: 'forbidden access'})
+      }
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+        if(err){
+          return res.status(401).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+      })
+    }
+
+
     // users related api
-    app.get('/users', async(req, res) =>{
+    app.get('/users', verifytoken, async(req, res) =>{
       const result = await userCollection.find().toArray();
       res.send(result)
     })
